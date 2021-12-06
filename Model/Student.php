8d51a2c8f5a0c5f3117adb9c768eee3f0b0ea3f3
StefanAmur <stefan.amur22@gmail.com>
2021-12-06 11:41:31 +0100

    added DataSource & Student classes

---
+++ Model/Student.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+require_once './Model/Teacher.php';
+
+class Student extends Teacher {
+
+    private int $class_id;
+    public function __construct(int $id, string $name, string $email, int $class_id) {
+        parent::__construct($id, $name, $email);
+        $this->$class_id = $class_id;
+    }
+
+    // private int $id;
+    // private string $name;
+    // private string $email;
+    // private int $class_id;
+
+    // public function __construct(int $id, string $name, string $email, int $class_id) {
+    //     $this->id = $id;
+    //     $this->name = $name;
+    //     $this->email = $email;
+    //     $this->class_id = $class_id;
+    // }
+
+    // public function get_id() {
+    //     return $this->id;
+    // }
+
+    // public function get_name() {
+    //     return $this->name;
+    // }
+
+
+    // public function get_email() {
+    //     return $this->email;
+    // }
+
+    // public function get_class_id() {
+    //     return $this->class_id;
+    // }
+}
