6fe4ed219978637e3f3fa828b893a5250314bdd8
StefanAmur <stefan.amur22@gmail.com>
2021-12-06 18:54:08 +0100

    added StudentController & student-view

--- Model/Student.php
+++ Model/Student.php
@@ -12,32 +12,7 @@ class Student extends Teacher {
         $this->$class_id = $class_id;
     }
 
-    // private int $id;
-    // private string $name;
-    // private string $email;
-    // private int $class_id;
-
-    // public function __construct(int $id, string $name, string $email, int $class_id) {
-    //     $this->id = $id;
-    //     $this->name = $name;
-    //     $this->email = $email;
-    //     $this->class_id = $class_id;
-    // }
-
-    // public function get_id() {
-    //     return $this->id;
-    // }
-
-    // public function get_name() {
-    //     return $this->name;
-    // }
-
-
-    // public function get_email() {
-    //     return $this->email;
-    // }
-
-    // public function get_class_id() {
-    //     return $this->class_id;
-    // }
+    public function getClassId() {
+        return $this->class_id;
+    }
 }
--- index.php
+++ index.php
@@ -1,25 +1,26 @@
 <?php
+
 declare(strict_types=1);
 
-
-require 'Model/DotEnv.php';
-$env = new DotEnv(__DIR__ . '/.env');
-$env -> load();
-
-
 //include all your model files here
-require 'Model/User.php';
+require 'Model/DotEnv.php';
+require 'Model/Student.php';
+require 'Model/StudentLoader.php';
 //include all your controllers here
 require 'Controller/HomepageController.php';
 require 'Controller/InfoController.php';
+require 'Controller/StudentController.php';
 
 //you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
 //this file should never be more than 20 lines of code!
+$env = new DotEnv(__DIR__ . '/.env');
+$env->load();
 
 $controller = new HomepageController();
-if(isset($_GET['page']) && $_GET['page'] === 'info') {
+if (isset($_GET['page']) && $_GET['page'] === 'info') {
     $controller = new InfoController();
+} elseif (isset($_GET['page']) && $_GET['page'] === 'student-view') {
+    $controller = new StudentController();
 }
-
 
 $controller->render($_GET, $_POST);
