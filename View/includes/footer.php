725e4aaf68b90b1f11997ca8d39330b0205230da
StefanAmur <stefan.amur22@gmail.com>
2021-12-09 22:31:18 +0100

    removed comments, added underline for footer links

--- View/includes/footer.php
+++ View/includes/footer.php
@@ -1,11 +1,11 @@
     <footer class="footer-container">
         &copy; BeCode <?php echo date('Y') ?>
         <span class="footer-span">&#124;</span>
-        <a href="https://github.com/N-Kaplan" class="link" target="_blank" rel="noreferrer">Naama Kaplan</a>
+        <a href="https://github.com/N-Kaplan" class="link hover-underline" target="_blank" rel="noreferrer">Naama Kaplan</a>
         <span class="footer-span">&#124;</span>
-        <a href="https://github.com/itsme17" class="link" target="_blank" rel="noreferrer">Binayak Shrestha</a>
+        <a href="https://github.com/itsme17" class="link hover-underline" target="_blank" rel="noreferrer">Binayak Shrestha</a>
         <span class="footer-span">&#124;</span>
-        <a href="https://github.com/StefanAmur" class="link" target="_blank" rel="noreferrer">Stefan Amuraritei</a>
+        <a href="https://github.com/StefanAmur" class="link hover-underline" target="_blank" rel="noreferrer">Stefan Amuraritei</a>
     </footer>
     </body>
 
