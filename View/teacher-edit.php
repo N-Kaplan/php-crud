c29700afb75593951deb207be7f1a45286ca21ee
Naama Kaplan <nkaplany@gmail.com>
2021-12-09 09:00:34 +0100

    minor adjustment view

--- View/teacher-edit.php
+++ View/teacher-edit.php
@@ -29,8 +29,9 @@
 <section>
     <h4>Edit teacher information:</h4>
     <form method="post" action="">
+
         <label for="teacher-name">Name: </label>
-        <input type="text" name="teacher-name" placeholder="Name" pattern="(^[a-zA-Z][a-zA-Z\s]{0,20}[a-zA-Z]$)" required>
+        <input type="text" name="teacher-name" placeholder=<php?$_POST[]?>"" pattern="(^[a-zA-Z][a-zA-Z\s]{0,20}[a-zA-Z]$)" required>
         <label for="teacher-email">Email: </label>
         <input type="email" name="teacher-email" placeholder="email" required>
         <button type="submit" name="add" value="add">Add</button>
